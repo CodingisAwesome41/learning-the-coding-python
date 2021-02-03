8f4144b7f912e47ccfad554ebcc14ef2c7ea95c6
Sameul Soni <sonisomil41@gmail.com>
2021-02-03 16:23:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+somil = input("What is your name?")
+
+clas = input("In which class do you study?")
+
+friend = input("What is your best friend name = ")\
+
+print(" Your name is ", somil,
+ ". You study in class = ", clas,  "th",
+  ". Your best friend name is ",friend)
